ef837af6eca6e6d33144589b52b068f1816fa2d8
Marcel <piet25@freenet.de>
2018-01-15 20:02:01 +0100

    add button and method to change waveforms

--- AudioTest/ViewController.swift
+++ AudioTest/ViewController.swift
@@ -56,6 +56,12 @@ class ViewController: UIViewController {
     var maxValX = 0.00
     var maxValY = 0.00
     
+    var wave = AKTable()
+    
+    var amp = 0.5
+    var freq = 100.0
+    var ramp = 0.0
+    
     var status = Status.Gyroskop
     
     @IBOutlet weak var audioPlot: UIView!
@@ -99,6 +105,33 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func waveformButton(_ sender: UIButton) {
+        switch sender.title(for: .normal)! {
+        case "Sine":
+            loadAudio(sine)
+            sender.setTitle("Square", for: .normal)
+            print("Sine")
+            setupPlot()
+        case "Square":
+            loadAudio(square)
+            sender.setTitle("Triangle", for: .normal)
+            print("Square")
+            setupPlot()
+        case "Triangle":
+            loadAudio(triangle)
+            sender.setTitle("Sawtooh", for: .normal)
+            print("Triangle")
+            setupPlot()
+        case "Sawtooh":
+            loadAudio(sawtooth)
+            sender.setTitle("Sine", for: .normal)
+            print("Sawtooh")
+            setupPlot()
+        default:
+            print("No waveforms!")
+        }
+    }
+  
     //Loop through the different sensors bei pressing the button
     @IBAction func modeButton(_ sender: UIButton) {
         switch sender.title(for: .normal)! {
@@ -131,22 +164,31 @@ class ViewController: UIViewController {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        oscillator = AKOscillator(waveform: square)
+    
+    func loadAudio(_ wave: AKTable) {
+        oscillator.stop()
+        AudioKit.stop()
+        oscillator = AKOscillator(waveform: wave)
         AudioKit.output = oscillator
         AudioKit.start()
         oscillator.start()
-        oscillator.rampTime = 0.2
+        changeSoundAmplitude(amplitude: amp)
+        changeSoundFrequency(frequency: freq)
+        changeSoundRampTime(rampTime: ramp)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        //oscillator = AKOscillator(waveform: square)
+        //loadAudio(sine)
         
         //motionManager.startAccelerometerUpdates()
         motionManager.startGyroUpdates()
         
         timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
         setupPlot()
-        
-        
     }
     
     func setupPlot() {
@@ -189,6 +231,7 @@ class ViewController: UIViewController {
     func changeSoundFrequency(frequency: Double) {
         if (frequency <= maxFrequency) {
             oscillator.frequency = frequency
+             freq = frequency
             //oscillator.baseFrequency = frequency
         }else {
             print("Frequency to High")
@@ -197,7 +240,8 @@ class ViewController: UIViewController {
     
     func changeSoundAmplitude(amplitude: Double) {
         if (amplitude <= maxAmplitude) {
-            //oscillator.amplitude = amplitude
+            oscillator.amplitude = amplitude
+            amp = amplitude
         }else {
             print("Amplitude to High")
         }
@@ -207,6 +251,7 @@ class ViewController: UIViewController {
     func changeSoundRampTime(rampTime: Double) {
         if (rampTime <= maxRampTime) {
             oscillator.rampTime = rampTime
+            ramp = rampTime
         }else {
             print("Ramp Time to High")
         }
